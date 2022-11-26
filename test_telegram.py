84771bfd954da6651d2aca9b51130c0c01705107
MinhNhan <nvminhnhan@gmail.com>
2022-11-27 00:44:53 +0700

    add telegram bot

---
+++ test_telegram.py
@@ -0,0 +1,12 @@
+import telegram
+
+#this will send to Nhan's telegram
+my_token = "5811414502:AAGx1n_NTi8ev93uD9cZLXLiRZYsvGJRB5s"
+
+# Tạo bot
+bot = telegram.Bot(token=my_token)
+
+
+# Gửi thử text message
+# bot.sendMessage(chat_id = "", text="Gưi từ PyCharm")
+bot.sendPhoto(chat_id="1871882149", photo = open("b.jpeg","rb"), caption = "Có súng, nguy hiêm!")
